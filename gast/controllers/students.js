c308b7a5908d0021fe712a0b9ac7bf3b74df272c
maria0alawadhi <mariaalawadhi@gmail.com>
2024-05-24 18:17:56 +0300

    added models

---
+++ gast/controllers/students.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const studentsCtrl = require('../controllers/students');
+
+router.get('/', studentsCtrl.getAll);
+router.get('/new', studentsCtrl.new);
+router.get('/:id', studentsCtrl.show);
+router.post('/', studentsCtrl.create);
+
+
+
+
+module.exports = router;
---
+++ gast/models/student.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  homework: { type: [Number] },
+  project: { type: [Number] },
+  attendanceObject: {
+    date: { type: Date, required: true },
+    status: {
+      type:Boolean,
+      enum: ['true', 'false'],
+      required: true
+    }
+  }}, 
+  {
+    timestamps: true
+  });
+
+module.exports = mongoose.model('Student', studentSchema);
