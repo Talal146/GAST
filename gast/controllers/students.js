fbe234d9c26f1d9c0fcb4b4f86a4110a0888eaa7
maria0alawadhi <mariaalawadhi@gmail.com>
2024-05-26 15:50:20 +0300

    grades file  is changd

--- gast/controllers/students.js
+++ gast/controllers/students.js
@@ -35,7 +35,7 @@ const create = async(req, res) => {
 const index = async (req, res, next) => {
   try {
       const students = await Student.find({});
-      res.render('students/', {students});
+      res.render('students/index', {students});
       console.log(students)
   }
   catch (err) {
