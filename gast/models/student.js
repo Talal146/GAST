eb0a4134cecee6e0f3dd364a013b53a6fe5cce27
Talal146 <talal.abdullah146@gmail.com>
2024-05-27 14:15:36 +0300

    fixing oath

--- gast/models/student.js
+++ gast/models/student.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const studentSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: Number },
-  details: { type:Boolean},
-  homework: { type: [Number] },
-  project: { type: [Number] },
-  attendanceObject: {
-    date: { type: Date},
-    status: {
-      type:Boolean,
-      enum: ['true', 'false'],
+const studentSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number },
+    details: { type: Boolean },
+    homework: [{ type: Schema.Types.ObjectId, ref: 'homework' }],
+    project: { type: [Number] },
+    attendanceObject: {
+      date: { type: Date },
+      status: {
+        type: Boolean,
+        enum: ['true', 'false']
+      }
     }
-  }}, 
+  },
   {
     timestamps: true
-  });
+  }
+)
 
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model('Student', studentSchema)
