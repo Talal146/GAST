ac7d33915a7477d26990c2b1de254027519e707c
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-05-27 00:33:13 -0700

    create function controller for homework

--- gast/controllers/homeworks.js
+++ gast/controllers/homeworks.js
@@ -1,4 +1,6 @@
 var homeworkModel = require('../models/homework');
+const Student = require('../models/student');
+const User = require('../models/user');
 
 const index = async (req, res) => {
 	try {
@@ -9,6 +11,32 @@ const index = async (req, res) => {
 		});
 	} catch {}
 };
+
+const newhomework = (req, res) => {
+	res.render('homeworks/new');
+}
+
+const create = async(req, res) => {
+    try {
+	  homeworkModel.create(req.body)
+      req.body.details = !!req.body.details;
+  
+      if (req.body.name) {
+        req.body.name = req.body.name.trim();
+        req.body.name = req.body.name.split(/\s*,\s*/);
+      }
+  
+     await homeworkModel.save();
+      console.log(student);         
+	  res.redirect('/homeworks');
+       } catch (err) {
+      console.error(err);
+	  res.redirect('/homeworks/new');
+        }
+  }
+
 module.exports = {
 	index,
+	new: newhomework,
+	create
 };
