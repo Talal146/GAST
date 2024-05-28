770d44ee3358e02e57a27765565f3bf575cd4dec
maria0alawadhi <mariaalawadhi@gmail.com>
2024-05-28 10:30:50 +0300

    fix the navbar

--- gast/routes/students.js
+++ gast/routes/students.js
@@ -4,17 +4,10 @@ const router = express.Router();
 const studentsCtrl = require('../controllers/students');
 
 router.get('/', studentsCtrl.index);
-
 router.get('/new', studentsCtrl.new);
-
-router.get('/grade', studentsCtrl.getStudents);
-router.post('/grade', studentsCtrl.submitGrades);
-
-router.get('/attendance', studentsCtrl.getStudents2);
+router.get('/attendance', studentsCtrl.getStudents);
 router.post('/attendance', studentsCtrl.submitAttendance);
-
 router.post('/', studentsCtrl.create);
-
 router.get('/:id', studentsCtrl.show);
 
 module.exports = router;
