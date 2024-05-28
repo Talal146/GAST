5461aceab4cadaf7959962b1ca0dd0a2429877f4
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-05-28 06:06:08 -0700

    checking  the functionality of the project

--- gast/routes/homeworks.js
+++ gast/routes/homeworks.js
@@ -7,6 +7,6 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 router.get('/', ensureLoggedIn,homeworksCtrl.index);
 router.get('/new',ensureLoggedIn, homeworksCtrl.new);
 router.post('/', ensureLoggedIn,homeworksCtrl.create);
-router.get('/:id'ensureLoggedIn,homeworksCtrl.show);
+router.get('/:id',ensureLoggedIn,homeworksCtrl.show);
 
 module.exports = router;
