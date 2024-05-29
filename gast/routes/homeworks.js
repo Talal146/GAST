2871d6780a6d72b7a4ac8be75ca6bf6f0bf173da
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-05-29 03:55:42 -0700

    fix

--- gast/routes/homeworks.js
+++ gast/routes/homeworks.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const homeworksCtrl = require('../controllers/homeworks');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
+router.get('/', ensureLoggedIn,homeworksCtrl.index);
 router.get('/new',ensureLoggedIn, homeworksCtrl.new);
 router.post('/', ensureLoggedIn,homeworksCtrl.create);
 router.get('/:id',ensureLoggedIn,homeworksCtrl.show);
+
 module.exports = router;
